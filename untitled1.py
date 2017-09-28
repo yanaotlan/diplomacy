0489440af4dd85b4704fcce051413eac91acb404
Yana Otlan <otlanyana@gmail.com>
2017-09-28 08:57:41 -0700

    testing things

---
+++ untitled1.py
@@ -0,0 +1,24 @@
+#import nltk
+import pandas as pd
+import numpy as np
+import random
+import csv
+
+### Extract participants
+
+#1.Iterate over the entries in the raw file 
+#2.Identify titles that include entries such as "ambassador of country A", "president of country B"
+#3.Add participants (names if possible and country of origin) to the file
+#   3.1. format: sender country| sender name | receiver country | receiver name
+
+#take a random sample from the big raw file to work with
+def test_sample(file, size):
+    #read the raw file
+    #with open('raw.csv', 'r') as csvfile:
+    #find out the number of rows
+    #define the sample size
+    #sample random rows
+    
+
+    
+    
